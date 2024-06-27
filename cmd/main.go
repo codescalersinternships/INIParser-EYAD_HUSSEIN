048b343be08a8d7daa7e7a993844f637fef02e85
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-06-27 22:05:06 +0300

    fix: fix lint issues in main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"log"
+
 	iniparser "github.com/codescalersinternships/INIParser-EYAD_HUSSEIN/pkg"
 )
 
 func main() {
 	parser := iniparser.IniParser{}
-	parser.LoadFromFile("./data.ini")
+	err := parser.LoadFromFile("./data.ini")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
